dcaaf62c671228fd875035d43db062fe65c8c92a
Igor Filler <igor_filler@epam.com>
2016-07-06 15:50:05 +0300

    Lock ReentranLock

--- Concurrency/com/epam/main/Account.java
+++ Concurrency/com/epam/main/Account.java
@@ -1,21 +1,40 @@
 package com.epam.main;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by Igor_Filler on 7/5/2016.
  */
 public class Account {
+    private Lock lock;
+
     private int balance;
 
+    public Lock getLock() {
+        return lock;
+    }
+
+    public void setLock(Lock lock) {
+        this.lock = lock;
+    }
+
+    public Account() {
+    }
+
     public Account(int balance) {
+        lock = new ReentrantLock();
         this.balance = balance;
     }
 
-    public void withdrow(int amount){
-        balance-=amount;
+    public void withdrow(int amount) {
+        balance -= amount;
     }
-    public void deposit(int amount){
-        balance+=amount;
+
+    public void deposit(int amount) {
+        balance += amount;
     }
+
     public int getBalance() {
         return balance;
     }
--- Concurrency/com/epam/main/Operations.java
+++ Concurrency/com/epam/main/Operations.java
@@ -1,33 +1,71 @@
 package com.epam.main;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
 import javax.naming.InsufficientResourcesException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Igor_Filler on 7/5/2016.
  */
 public class Operations {
-    public static void main(String[] args) throws InsufficientFundsException {
+    final static Logger logger = Logger.getLogger(Operations.class);
+    public static void main(String[] args) throws InsufficientFundsException, InterruptedException {
         final Account a = new Account(1000);
         final Account b = new Account(2000);
 
         new Thread(new Runnable() {
             @Override
-            public void run()  {
+            public void run() {
                 try {
                     transfer(a, b, 500);
                 } catch (InsufficientFundsException e) {
                     e.printStackTrace();
+                } catch (InterruptedException e) {
+                    logger.error("InterruptedException",e);
+                    e.printStackTrace();
                 }
             }
         }).start();
-        transfer(b,a,300);
+        transfer(b, a, 300);
     }
-    static void transfer(Account a1, Account a2, int amount) throws InsufficientFundsException{
-        if (a1.getBalance()< amount){
+
+    static void transfer(Account a1, Account a2, int amount) throws InsufficientFundsException, InterruptedException {
+
+        if (a1.getBalance() < amount) {
             throw new InsufficientFundsException("Balance trouble");
         }
-        a1.withdrow(amount);
-        a2.deposit(amount);
+        if (a1.getLock().tryLock(3, TimeUnit.MILLISECONDS)){
+            try{
+                logger.error("First lock!");
+                if(a2.getLock().tryLock(3, TimeUnit.MILLISECONDS)){
+                  try{
+                      logger.error("Transfer!");
+                      a1.withdrow(amount);
+                      a2.deposit(amount);
+                  }finally {
+                      a2.getLock().unlock();
+                  }
+                }else {
+                    logger.error("Sorry!");
+                }
+            } finally {
+                a1.getLock().unlock();
+            }
+        } else {
+            logger.error("Sorry!");
+        }
+/*        synchronized (a1) {
+            Thread.sleep(1000);
+            logger.debug("First syncro");
+           // System.out.println("First syncro");
+            synchronized (a2) {
+                System.out.println("Second syncro");
+                a1.withdrow(amount);
+                a2.deposit(amount);
+            }
+        }*/
     }
 
     private static class InsufficientFundsException extends Throwable {
